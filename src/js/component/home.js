0da9712ccbe1dac6dfc461ea0943f872b6885015
Anmora96 <anmora.2396@gmail.com>
2021-02-15 04:53:55 +0000

    Comprendiendo más sobre React

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,69 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//Creando el Traffic Light desde Home
+export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			//Aqui se va a indicar el estado inicial
+			clicked: "red"
+		};
+	}
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	render() {
+		let newRedLightClass = "";
+		let newYellowLightClass = "";
+		let newGreenLightClass = "";
+
+		if (this.state.clicked == "red") {
+			newRedLightClass = "light-glow-red";
+		} else if (this.state.clicked == "yellow") {
+			newYellowLightClass = "light-glow-yellow";
+		} else if (this.state.clicked == "green") {
+			newGreenLightClass = "light-glow-green";
+		}
+
+		return (
+			//return siempre recordar que todo va dentro del parentesis, no dentro de "corchetas"
+			<div className="container">
+				<div className="row justify-content-center align-items-center">
+					<div className="traffic-light-container p-3 mt-5 justify-content-center">
+						<div
+							className={
+								"mb-1 traffic-light-red rounded-circle" +
+								" " +
+								newRedLightClass
+							}
+							onClick={() =>
+								this.setState({
+									clicked: "red"
+								})
+							}></div>
+						<div
+							className={
+								"mb-1 traffic-light-yellow rounded-circle" +
+								" " +
+								newYellowLightClass
+							}
+							onClick={() =>
+								this.setState({
+									clicked: "yellow"
+								})
+							}></div>
+						<div
+							className={
+								"traffic-light-green rounded-circle" +
+								" " +
+								newGreenLightClass
+							}
+							onClick={() =>
+								this.setState({
+									clicked: "green"
+								})
+							}></div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
